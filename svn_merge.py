c64dcb0195e9dd45ed523bc24a6fd8b133dd46a7
Annamalai Arunachalam <aa00482762@techmahindra.com>
2018-11-01 18:31:07 +0530

    File Modified for password Mask

--- svn_merge.py
+++ svn_merge.py
@@ -3,19 +3,26 @@
 '''
 Creating this code to Merge codes between SVN Branches
 
-Created by BroMount 1 Nov 2018
+Created by BroMount on 1 Nov 2018
+
+Contributors
+
+Archana Elangovan
 
 '''
 
 import pysvn
+import getpass
 
 client = pysvn.Client()
 
 def get_login( realm, username, may_save ):
     name = raw_input ("Enter your SVN username : ")
-    password = raw_input("Enter your password : ")
+    password = getpass.getpass("Enter your password : ")
     return True,name,password,False
 
 client.callback_get_login = get_login
-client.checkout('https://URL','./examples/pysvn')
+client.checkout('https://URL','./repository/branches')
+
+
 
