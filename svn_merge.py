68a2d0e3f2c475c25a8cc6513329554686668ea2
Annamalai Arunachalam Arumugam <iambromount@gmail.com>
2018-11-02 13:33:17 +0530

    Updated svn_merge.py with SVN log and timestamp conversion

--- svn_merge.py
+++ svn_merge.py
@@ -5,16 +5,15 @@ Creating this code to Merge codes between SVN Branches
 
 Created by BroMount on 1 Nov 2018
 
-Contributors
-
-Archana Elangovan
-
 '''
 
 import pysvn
 import getpass
+from datetime import datetime
 
 client = pysvn.Client()
+
+url = 'URL'
 
 def get_login( realm, username, may_save ):
     name = raw_input ("Enter your SVN username : ")
@@ -22,7 +21,11 @@ def get_login( realm, username, may_save ):
     return True,name,password,False
 
 client.callback_get_login = get_login
-client.checkout('https://URL','./repository/branches')
+#client.checkout(URL,'./repository/branches')
 
+commit_messages = client.log(url)
+for i, commit in enumerate(commit_messages):
+    commit_time = datetime.utcfromtimestamp(commit.date).strftime('%Y-%m-%d %H:%M:%S')
+    print i, commit.message, commit.revision, commit.author, commit_time
 
 
