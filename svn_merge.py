3759d09ff33baed1d447916de8c5ea1ad08a1143
Annamalai Arunachalam Arumugam <iambromount@gmail.com>
2018-11-03 11:09:50 +0530

    adding commit message to text file and REGEX to find keyword

--- svn_merge.py
+++ svn_merge.py
@@ -27,5 +27,30 @@ commit_messages = client.log(url)
 for i, commit in enumerate(commit_messages):
     commit_time = datetime.utcfromtimestamp(commit.date).strftime('%Y-%m-%d %H:%M:%S')
     print i, commit.message, commit.revision, commit.author, commit_time
+    file1 = open("./text/commit_messages.txt","a")
+    file1.write("{}\n".format(text))
+    file1.close()
+
+print "Text file with commit messag created"
+
+message_file = open("./text/commit_messages.txt","r")
+lines = message_file.read()
+
+#commit_key = re.search('aarumug',lines)
+
+commit_key = re.findall("aarumug2",lines)
+
+for i in commit_key:
+    print commit_key
+
+#print commit_key
+'''
+if commit_key in None:
+    print "No keyword found"
+else:
+    print commit_key.group(1)
+'''    
+    
+
 
 
