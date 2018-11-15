bdd26246f35e3928d99a003bf03518a5b9046576
Annamalai Arunachalam <aa00482762@techmahindra.com>
2018-11-15 07:12:24 +0530

    Adding one local file

---
+++ os_commands.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os
+
+
+os.system ("pwd")
+
+os.chdir ("./repository")
+
+os.system ("pwd")
+
+os.system ("cd /home/bromount/MyWorks/svn_merge/repository/")
+
+os.system ("pwd")
